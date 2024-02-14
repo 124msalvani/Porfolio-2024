f69fa77e19a81cc18cf586a9e18bbd0c994a7f9e
124msalvani <124msalvani@frhsd.com>
2024-02-14 10:50:08 -0500

    Made js, when you click on genshin it takes you to website

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("exampleWebsite").addEventListener('click', githubLink);
+function githubLink() {
+    location.href = 'https://124msalvani.github.io/Genshin-Guide/';
+
+    
+  };
